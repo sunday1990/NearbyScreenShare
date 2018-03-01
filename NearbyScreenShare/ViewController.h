42426e24e0b6ef0a851b8a6b6c29260cf68bd054
ccSunday <935143023@qq.com>
2018-03-01 09:17:47 +0800

    NearbyScreenShare首次提交

---
+++ NearbyScreenShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NearbyScreenShare
+//
+//  Created by ccSunday on 2018/3/1.
+//  Copyright © 2018年 ccSunday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NearbyScreenShare/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NearbyScreenShare
+//
+//  Created by ccSunday on 2018/3/1.
+//  Copyright © 2018年 ccSunday. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
